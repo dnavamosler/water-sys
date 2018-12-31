9afe90e8dce4eb9a7c6a749226d4529a28e22ea5
daniel nava <dnavamoslercl@gmail.com>
2018-12-30 21:37:26 -0300

    Updates

---
+++ precache-manifest.9680348abf684a91a129b1cee1b21464.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "289d54792a35eed32675",
+    "url": "/water-sys/static/css/main.87eed267.chunk.css"
+  },
+  {
+    "revision": "289d54792a35eed32675",
+    "url": "/water-sys/static/js/main.289d5479.chunk.js"
+  },
+  {
+    "revision": "f739feb7d61c48a46bed",
+    "url": "/water-sys/static/js/1.f739feb7.chunk.js"
+  },
+  {
+    "revision": "10b2c9f454ccd3f7270a",
+    "url": "/water-sys/static/js/runtime~main.10b2c9f4.js"
+  },
+  {
+    "revision": "7de2d72722c8ac9671594d7a7b4d83bf",
+    "url": "/water-sys/index.html"
+  }
+];
