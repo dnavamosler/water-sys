66a1a65f918319bd4d08605ab4ff1a1a09ad821e
daniel nava <dnavamoslercl@gmail.com>
2019-01-05 15:32:20 -0300

    resuelto bug de login

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -3,23 +3,23 @@ import './footer.css'
 
 
 const Footer = (props) => {
-    
+
 
     //logica para conseguir si el usuario que ingresa es administrador o usuario comun
-    const typeUser = (usuario) =>{
+    const typeUser = (usuario) => {
 
-       ( usuario == 'user')
-
-        ? ( <h2>  ingresar como usuario </h2>)
-        : (<h2>  ingresar como {props.user} </h2>)
+        if (usuario == 'user')
+            return (<h2>  ingresar como usuario </h2>)
+        else
+            return (<h2>  ingresar como {props.user} </h2>)
     }
-    
-    
-    return(
+
+
+    return (
         <div >
-            
-                {typeUser(props.user)}                
-            
+
+            {typeUser(props.user)}
+
         </div>
     )
 }
